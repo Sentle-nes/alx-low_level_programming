7577f3b5a23d397a7ca109106143d9661e721790
Sentle-nes <sentledintle@gmail.com>
2023-03-17 10:00:20 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function 
- * Return - always a 0
- */
+ * main - prints "Programming is like building a multilingual puzzle,
+ *  Followed by a new line"
+ * Return - 0.
+*/
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts ("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
