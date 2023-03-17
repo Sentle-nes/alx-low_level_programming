0be3294583c45600ddc76b826cfd895ac308c004
Sentle-nes <sentledintle@gmail.com>
2023-03-17 10:08:05 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - prints "Programming is like building a multilingual puzzle,
  *  Followed by a new line"
@@ -7,6 +8,7 @@
 */
 
 int main(void)
+
 {
 	puts ("\"Programming is like building a multilingual puzzle");
 
